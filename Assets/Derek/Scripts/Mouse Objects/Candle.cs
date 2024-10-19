9c1ebc3274e784b6274a7b5eb19af572d6c86a2e
Derek <dereksturm263@gmail.com>
2024-10-19 15:18:31 -0700

    - Created placeholder scripts for possible mouse hiding spots - Implemented the Bookshelf, which will have a single book shake if there's a mouse in it - Added a Range<T> type to represent a range of values of type T

---
+++ Assets/Derek/Scripts/Mouse Objects/Candle.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+[AddComponentMenu("Mouse Objects/Candle")]
+public class Candle : MouseObject
+{
+    public override void OnStartWithMouse(GameObject mouse)
+    {
+
+    }
+
+    public override void OnStartWithoutMouse()
+    {
+
+    }
+
+    public override void OnUpdateWithMouse(GameObject mouse)
+    {
+
+    }
+
+    public override void OnUpdateWithoutMouse()
+    {
+
+    }
+
+    public override void OnEndWithMouse(GameObject mouse)
+    {
+
+    }
+
+    public override void OnEndWithoutMouse()
+    {
+
+    }
+
+    public override void OnSetup()
+    {
+
+    }
+
+    public override void OnClear()
+    {
+
+    }
+}
---
+++ Assets/Derek/Scripts/Mouse Objects/MouseObject.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Animator))]
+public abstract class MouseObject : MonoBehaviour, IInteractable, ISetup
+{
+    [SerializeField] private UnityEvent<GameObject> _onInteract;
+
+    private bool _hasMouseCurrently;
+    private GameObject _mouse;
+
+    private Animator _anim;
+
+    public void AddMouse()
+    {
+        if (_hasMouseCurrently || _mouse)
+            return;
+
+        _hasMouseCurrently = true;
+        _mouse = Instantiate(MouseObjectManager.Instance.MousePrefab);
+
+        OnEndWithoutMouse();
+        OnStartWithMouse(_mouse);
+    }
+
+    public void RemoveMouse()
+    {
+        if (!_hasMouseCurrently || !_mouse)
+            return;
+
+        OnEndWithMouse(_mouse);
+        OnStartWithoutMouse();
+
+        _hasMouseCurrently = false;
+        Destroy(_mouse);
+        _mouse = null;
+    }
+    
+    protected virtual void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        MouseObjectManager.Instance.AllMouseObjects.Add(this);
+    }
+
+    private void Update()
+    {
+        if (_hasMouseCurrently)
+        {
+            OnUpdateWithMouse(_mouse);
+        }
+        else
+        {
+            OnUpdateWithoutMouse();
+        }
+    }
+
+    public abstract void OnStartWithMouse(GameObject mouse);
+    public abstract void OnUpdateWithMouse(GameObject mouse);
+    public abstract void OnEndWithMouse(GameObject mouse);
+
+    public abstract void OnStartWithoutMouse();
+    public abstract void OnUpdateWithoutMouse();
+    public abstract void OnEndWithoutMouse();
+
+    public void OnInteract(GameObject player)
+    {
+        Debug.Log($"{name} was interacted with by {player.name}");
+
+        _anim.SetTrigger("Interact");
+        _onInteract.Invoke(player);
+    }
+
+    public abstract void OnSetup();
+    public abstract void OnClear();
+}
